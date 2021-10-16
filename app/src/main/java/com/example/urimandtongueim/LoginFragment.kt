7318d936c14df3bf44e72bcd9db589755c59a367
JJ Thorley <thorjj98@byu.edi>
2021-10-16 11:44:33 -0600

    login async

--- app/src/main/java/com/example/urimandtongueim/LoginFragment.kt
+++ app/src/main/java/com/example/urimandtongueim/LoginFragment.kt
@@ -1,6 +1,9 @@
 package com.example.urimandtongueim
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.AsyncTask
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,11 +11,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.FragmentManager
 import com.example.urimandtongueim.model.DataCache
 import com.example.urimandtongueim.model.requests.LoginRequest
+import com.example.urimandtongueim.model.requests.RegisterRequest
+import com.example.urimandtongueim.model.responses.LoginResponse
+import com.example.urimandtongueim.model.responses.RegisterResponse
 import com.example.urimandtongueim.model.service.LanguageService
 import com.example.urimandtongueim.model.service.LoginService
+import com.example.urimandtongueim.model.service.RegisterService
 
 class LoginFragment : Fragment() {
 
@@ -22,6 +30,7 @@ class LoginFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -35,21 +44,7 @@ class LoginFragment : Fragment() {
             val userName = view.findViewById<EditText>(R.id.editUsernameText).toString()
             val password = view.findViewById<EditText>(R.id.editPasswordText).toString()
 
-            val response = loginService.login(LoginRequest(userName, password))
-            if (response != null) {
-                if (response.isSuccess()){
-                    DataCache.setLoggedInStatus(true)
-                    val fm: FragmentManager? = fragmentManager
-                    val homeFragment: HomeFragment = HomeFragment()
-                    val args = Bundle()
-                    homeFragment.arguments = args
-                    if (fm != null) {
-                        fm.beginTransaction()
-                            .replace(R.id.fragmentContainer, homeFragment)
-                            .commit()
-                    }
-                }
-            }
+            LoginTask().execute(LoginRequest(userName, password))
         }
 
         val registerButton = view.findViewById<Button>(R.id.registerButton)
@@ -66,5 +61,37 @@ class LoginFragment : Fragment() {
         }
 
         return view
+    }
+
+    fun login(response: LoginResponse) {
+        if (response.isSuccess()){
+            DataCache.setLoggedInStatus(true)
+            val fm: FragmentManager? = fragmentManager
+            val homeFragment: HomeFragment = HomeFragment()
+            val args = Bundle()
+            homeFragment.arguments = args
+            if (fm != null) {
+                fm.beginTransaction()
+                    .replace(R.id.fragmentContainer, homeFragment)
+                    .commit()
+            }
+        }
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    inner class LoginTask @RequiresApi(Build.VERSION_CODES.N) constructor() :
+        AsyncTask<LoginRequest, Void, LoginResponse>() {
+
+        @RequiresApi(Build.VERSION_CODES.N)
+        override fun onPostExecute(response: LoginResponse) {
+            super.onPostExecute(response)
+            login(response)
+        }
+
+        override fun doInBackground(vararg request: LoginRequest): LoginResponse? {
+            val loginService = LoginService()
+            return loginService.login(request[0])
+        }
+
     }
 }
